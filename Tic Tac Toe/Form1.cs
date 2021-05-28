88339e2a881d1ea2a09e8147eea1f7522e32e57a
John Lester Rentosa <johnlesterfrentosa@iskolarngbayan.pup.edu.ph>
2021-05-28 14:36:43 +0800

    restart if win and arranging

--- Tic Tac Toe/Form1.cs
+++ Tic Tac Toe/Form1.cs
@@ -40,112 +40,129 @@ namespace Tic_Tac_Toe
                 switch(i)
                 {
                     case 0:
-                        combination = board[0] + board[4] + board[8];
+                        combination = board[1] + board[5] + board[9];
                         break;
                     case 1:
-                        combination = board[2] + board[4] + board[6];
+                        combination = board[2] + board[5] + board[7];
                         break;
                     case 2:
-                        combination = board[0] + board[1] + board[2];
+                        combination = board[1] + board[2] + board[3];
                         break;
                     case 3:
-                        combination = board[3] + board[4] + board[5];
+                        combination = board[4] + board[5] + board[6];
                         break;
                     case 4:
-                        combination = board[6] + board[7] + board[8];
+                        combination = board[7] + board[8] + board[9];
                         break;
                     case 5:
-                        combination = board[0] + board[3] + board[6];
+                        combination = board[1] + board[4] + board[7];
                         break;
                     case 6:
-                        combination = board[2] + board[4] + board[7];
+                        combination = board[2] + board[5] + board[8];
                         break;
                     case 7:
-                        combination = board[3] + board[5] + board[8];
+                        combination = board[3] + board[6] + board[9];
                         break;
                 }
 
                 if(combination.Equals("XXX"))
                 {
+                    restart();
                     MessageBox.Show("X wins! ", "We have a Winner", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 }
                 else if(combination.Equals("OOO"))
                 {
+                    restart();
                     MessageBox.Show("O wins! ", "We have a Winner", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 }
             }
                 
         }
 
+        public void restart()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            board = new string[10];
+            myTurn = 0;
+
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             myTurn++;
-            board[0] = returnSymbol(myTurn);
-            button1.Text = board[0];
+            board[1] = returnSymbol(myTurn);
+            button1.Text = board[1];
             Winner();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             myTurn++;
-            board[1] = returnSymbol(myTurn);
-            button2.Text = board[1];
+            board[2] = returnSymbol(myTurn);
+            button2.Text = board[2];
             Winner();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             myTurn++;
-            board[2] = returnSymbol(myTurn);
-            button3.Text = board[2];
+            board[3] = returnSymbol(myTurn);
+            button3.Text = board[3];
             Winner();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             myTurn++;
-            board[3] = returnSymbol(myTurn);
-            button4.Text = board[3];
+            board[4] = returnSymbol(myTurn);
+            button4.Text = board[4];
             Winner();
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             myTurn++;
-            board[4] = returnSymbol(myTurn);
-            button5.Text = board[4];
+            board[5] = returnSymbol(myTurn);
+            button5.Text = board[5];
             Winner();
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
             myTurn++;
-            board[5] = returnSymbol(myTurn);
-            button6.Text = board[5];
+            board[6] = returnSymbol(myTurn);
+            button6.Text = board[6];
             Winner();
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
             myTurn++;
-            board[6] = returnSymbol(myTurn);
-            button7.Text = board[6];
+            board[7] = returnSymbol(myTurn);
+            button7.Text = board[7];
             Winner();
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             myTurn++;
-            board[7] = returnSymbol(myTurn);
-            button8.Text = board[7];
+            board[8] = returnSymbol(myTurn);
+            button8.Text = board[8];
             Winner();
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
             myTurn++;
-            board[8] = returnSymbol(myTurn);
-            button9.Text = board[8];
+            board[9] = returnSymbol(myTurn);
+            button9.Text = board[9];
             Winner();
         }
     }
